7009099f0f37625efb4d7d6d7e8d1f27c35f70c7
Matheus Cohen <matheuscohen@hotmail.com>
2019-02-12 01:19:56 -0200

    Theme Portrait Ready/ Start Menu

--- Unity/Assets/Scripts/LevelData.cs
+++ Unity/Assets/Scripts/LevelData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class LevelData : ScriptableObject
 {
 
-    public int NumberOfWords;
-    public Vector2 GridSize;
-    public Section Theme;
+    public int NumberOfWords; //numero de palavras que devem existir no mapa
+    public Vector2 GridSize; //tamanho do mapa
+    public Section Theme; //tematica usada
 }
---
+++ Unity/Assets/SectionPortrait.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SectionPortrait : MonoBehaviour
+{
+
+    public Section section;
+
+    public TextMeshProUGUI Theme;
+    public TextMeshProUGUI Sample1;
+    public TextMeshProUGUI Sample2;
+    public TextMeshProUGUI Sample3;
+    public TextMeshProUGUI Sample4;
+
+    public Image sprite;
+
+    public void setPortrait(Section mySection)
+    {
+        section = mySection;
+        Theme.text = section.Theme;
+        sprite.sprite = section.ThemeImage;
+        Sample1.text = section.Words[0];
+        Sample2.text = section.Words[1];
+        Sample3.text = section.Words[2];
+        Sample4.text = section.Words[3];
+    }
+}
